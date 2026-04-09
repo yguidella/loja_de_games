300f2baa7d49f4f2978cb270c70d19442cebd9b3
yguidella <y.guidella8@outlook.com>
2026-04-08 22:49:01 -0300

    feat: findById

--- src/categoria/services/categoria.service.ts
+++ src/categoria/services/categoria.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Categoria } from "../entities/categoria.entity";
@@ -12,6 +12,17 @@ export class CategoriaService {
     
     async findAll(): Promise<Categoria[]>{
         return await this.categoriaRepository.find();
+    }
+
+    async findById(id: number): Promise<Categoria>{
+        let categoria = await this.categoriaRepository.findOne({
+            where: { id }
+        });
+
+        if (!categoria)
+            throw new HttpException('Categoria não encontrada!', HttpStatus.NOT_FOUND);
+        
+        return categoria;
     }
     
 }
