e2abb78665a5fa56014f8088adb1db0cf1ed3763
yguidella <y.guidella8@outlook.com>
2026-04-08 22:43:32 -0300

    feat: metodo findAll categoria service

---
+++ src/categoria/services/categoria.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Categoria } from "../entities/categoria.entity";
+
+@Injectable()
+export class CategoriaService {
+    constructor(
+        @InjectRepository(Categoria)
+        private categoriaRepository: Repository<Categoria>
+    ) {  }
+    
+    async findAll(): Promise<Categoria[]>{
+        return await this.categoriaRepository.find();
+    }
+    
+}
