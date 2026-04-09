f7addc7fe6ecd677041e49bacd861be34265a4d2
yguidella <y.guidella8@outlook.com>
2026-04-09 08:28:44 -0300

    feat: metodos find all byid bynome

---
+++ src/produto/services/produto.service.ts
@@ -0,0 +1,46 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
+import { Produto } from "../entities/produto.entity";
+
+@Injectable()
+export class ProdutoService {
+    constructor(
+        @InjectRepository(Produto)
+        private produtoRepository: Repository<Produto>
+
+    ){}
+
+    async findAll(): Promise<Produto[]> {
+        return await this.produtoRepository.find({
+            relations: {
+                categoria: true
+            }
+        });
+    }
+
+    async findById(id: number): Promise<Produto>{
+        let produto = await this.produtoRepository.findOne({
+            where: { id },
+            relations: {
+                categoria: true
+            }
+        });
+
+        if (!produto)
+            throw new HttpException ('Produto não encontrado!', HttpStatus.NOT_FOUND);
+        
+        return produto;
+    }
+
+    async findByNome(nome: string): Promise<Produto[]> {
+        return await this.produtoRepository.find({
+            where: {
+                nome: ILike(`%${nome}%`)
+            },
+            relations: {
+                categoria: true
+            }
+        });
+    }
+}
